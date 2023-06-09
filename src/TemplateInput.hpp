0490ee61e5558f6296311c08a8842490f75d8652
Georg Schwab <georg.schwab@xentara.io>
2023-06-09 11:44:58 +0200

    Updated for Xentara version 2.0

--- src/TemplateInput.hpp
+++ src/TemplateInput.hpp
@@ -4,10 +4,9 @@
 #include "ReadState.hpp"
 #include "ReadTask.hpp"
 
-#include <xentara/io/Io.hpp>
-#include <xentara/io/IoClass.hpp>
-#include <xentara/plugin/EnableSharedFromThis.hpp>
 #include <xentara/process/Task.hpp>
+#include <xentara/skill/DataPoint.hpp>
+#include <xentara/skill/EnableSharedFromThis.hpp>
 
 #include <functional>
 #include <string_view>
@@ -21,7 +20,7 @@ class TemplateIoComponent;
 
 /// @brief A class representing a specific type of input.
 /// @todo rename this class to something more descriptive
-class TemplateInput final : public io::Io, public plugin::EnableSharedFromThis<TemplateInput>
+class TemplateInput final : public skill::DataPoint, public skill::EnableSharedFromThis<TemplateInput>
 {
 private:
 	/// @brief A structure used to store the class specific attributes within an element's configuration
@@ -32,7 +31,7 @@ private:
 	
 public:
 	/// @brief The class object containing meta-information about this element type
-	class Class final : public io::IoClass
+	class Class final : public skill::Element::Class
 	{
 	public:
 		/// @brief Gets the global object
@@ -47,7 +46,7 @@ public:
             return _configHandle;
         }
 
-		/// @name Virtual Overrides for io::IoClass
+		/// @name Virtual Overrides for skill::Element::Class
 		/// @{
 
 		auto name() const -> std::string_view final
@@ -59,7 +58,7 @@ public:
 		auto uuid() const -> utils::core::Uuid final
 		{
 			/// @todo assign a unique UUID
-			return "cccccccc-cccc-cccc-cccc-cccccccccccc"_uuid;
+			return "deadbeef-dead-beef-dead-beefdeadbeef"_uuid;
 		}
 
 		/// @}
@@ -78,20 +77,20 @@ public:
 	{
 	}
 
-	/// @name Virtual Overrides for io::Io
+	/// @name Virtual Overrides for skill::DataPoint
 	/// @{
 	
 	auto dataType() const -> const data::DataType & final;
 
 	auto directions() const -> io::Directions final;
 
-	auto resolveAttribute(std::string_view name) -> const model::Attribute * final;
+	auto forEachAttribute(const model::ForEachAttributeFunction &function) const -> bool final;
 	
-	auto resolveTask(std::string_view name) -> std::shared_ptr<process::Task> final;
+	auto forEachEvent(const model::ForEachEventFunction &function) -> bool final;
 
-	auto resolveEvent(std::string_view name) -> std::shared_ptr<process::Event> final;
+	auto forEachTask(const model::ForEachTaskFunction &function) -> bool final;
 
-	auto readHandle(const model::Attribute &attribute) const noexcept -> data::ReadHandle final;
+	auto makeReadHandle(const model::Attribute &attribute) const noexcept -> std::optional<data::ReadHandle> final;
 
 	auto realize() -> void final;
 
@@ -103,13 +102,13 @@ public:
 	static const model::Attribute kValueAttribute;
 
 protected:
-	/// @name Virtual Overrides for io::Io
+	/// @name Virtual Overrides for skill::DataPoint
 	/// @{
 
 	auto loadConfig(const ConfigIntializer &initializer,
 		utils::json::decoder::Object &jsonObject,
 		config::Resolver &resolver,
-		const FallbackConfigHandler &fallbackHandler) -> void final;
+		const config::FallbackHandler &fallbackHandler) -> void final;
 
 	/// @}
 
